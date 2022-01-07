03fce56f163d90feb40b703f56deb1c519f0c43d
EdwardR7 <36159443+EdwardR7@users.noreply.github.com>
2022-01-07 14:25:13 +0100

    Create Items.java

---
+++ GenetiskAlgoritme/src/main/java/org/openjfx/GenetiskAlgoritme/Items.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openjfx.GenetiskAlgoritme;
+
+/**
+ *
+ * @author edwardrostomian
+ */
+public class Items {
+    
+    private float price;
+    
+     private float weight;
+    
+      private int type;
+     
+      
+      Items(){
+      
+      
+          
+      }
+      
+}
