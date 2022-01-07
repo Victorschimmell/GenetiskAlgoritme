b16c30ce69953a6a48a97b6e99a371007e57742b
NicklasKleemann <71381595+NicklasKleemann@users.noreply.github.com>
2022-01-07 13:58:44 +0100

    wasd

---
+++ GenetiskAlgoritme/src/main/java/org/openjfx/GenetiskAlgoritme/Population.java
@@ -0,0 +1,23 @@
+
+
+
+public class Population{
+
+ private float mutationRate; // mutation rate
+
+ private DNA[] population; // Array of current population
+
+ private ArrayList<DNA> matingPools; // Arraylist for mating pool
+
+ private int generations; // number of generations
+ 
+ private float Target; // Target value
+
+ private boolean finished; // Are we finish evolving?
+
+ private int perfectScore; // 
+
+Population(float MaxPrice, float mutation, int  )
+
+
+}
