205a0c7876959fbe00c50ab83a72cbdc6356396a
NicklasKleemann <71381595+NicklasKleemann@users.noreply.github.com>
2022-01-11 15:25:47 +0100

    wasd

--- GenetiskAlgoritme/src/main/java/org/openjfx/GenetiskAlgoritme/Menu.java
+++ GenetiskAlgoritme/src/main/java/org/openjfx/GenetiskAlgoritme/Menu.java
@@ -2,30 +2,49 @@ package org.openjfx.GenetiskAlgoritme;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Menu implements Initializable{
+public class Menu implements Initializable {
+
+    ArrayList<Integer> popPrice = new ArrayList<Integer>();
+    private int maxværdi;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
+        for (int i = 0; i < 8; i++) {
+            popPrice.add(Menu.randint(1, 8500));
+            System.out.println(popPrice.get(i));
+
+            try {
+                if (popPrice.get(i) > maxværdi) {
+                    maxværdi = popPrice.get(i);
+
+                    System.out.println("Maxværdi = " + maxværdi);
+                }
+
+            } catch (Exception e) {
+
+            }
+
+        }
+
     }
-    
-    
-    
+
     @FXML
     private void switchToSim() throws IOException {
-        
-        App.setRoot("Simulation");
-        
-    }
-    
-    public static int randint(int min, int max){
 
-    return ThreadLocalRandom.current().nextInt(min, max + 1);
+        App.setRoot("Simulation");
+
+    }
+
+    public static int randint(int min, int max) {
+
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 }
