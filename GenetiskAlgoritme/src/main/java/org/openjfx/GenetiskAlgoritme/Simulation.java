cbd0def2e5c829a2966f1074f3843915393e1492
EdwardR7 <36159443+EdwardR7@users.noreply.github.com>
2022-01-12 14:09:31 +0100

    LineChart opstilling

--- GenetiskAlgoritme/src/main/java/org/openjfx/GenetiskAlgoritme/Simulation.java
+++ GenetiskAlgoritme/src/main/java/org/openjfx/GenetiskAlgoritme/Simulation.java
@@ -3,6 +3,8 @@ package org.openjfx.GenetiskAlgoritme;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
@@ -13,15 +15,30 @@ public class Simulation implements Initializable {
 
      @FXML
     private LineChart LineChart;
+     
+     private int Data;
     
-       @Override
-    public void initialize(URL url, ResourceBundle rb) {
-              
+      
+     @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        
+          LineChart.dataProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+
+                LineChart = (LineChart) LineChart.getData();
+                
+            }
+          });
+                  
+                  
         Items blyat = new Items(Menu.randint(0,24));
         
         System.out.println(blyat.name + " : " + blyat.weight + " : " + blyat.price);
         
     }
+                  
    
 
     @FXML
